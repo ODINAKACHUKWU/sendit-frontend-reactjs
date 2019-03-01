f19aa54883eca4adfebd9d0b745a49fc46f014eb
Odinakachukwu Ezeobika <odinakachukwuezeobika@Odinakachukwus-MacBook-Pro.local>
2019-03-01 17:03:42 +0100

    chore(setup): setup empty react project
    - initialize project
    - setup webpack
    - configure babel
    
    [Finishes #164300943]

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/index.js"
+  },
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
